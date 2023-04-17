7246f893ac204d01c529083f9a7ea2add5f35731
gabrieldesousaalmeida <74242647+gabrieldesousaalmeida@users.noreply.github.com>
2023-04-17 10:10:38 -0300

    estrutura básica

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const app = express();
+
+//mongodb
+const mongoose = require('mongoose');
+
+// models
+require('./models/Artigo')
+const Artigo = mongoose.model('artigo')
+
+//conectando com banco de dados
+mongoose.connect('mongodb://0.0.0.0:27017/conteudo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('Conexão com banco de dados feita com sucesso')
+}).catch((erro)=>{
+    console.log('erro na conexão com banco de dados'+ erro)
+})
+
+// fazer o express permitir o uso de json
+app.use(express.json());
+
+//listando na API
+app.get('/', (req, res)=>{
+    const artigo = Artigo.find({})
+    .then((artigos)=>{
+        return res.json(artigos)
+    }).catch((error)=>{
+        return res.status(400).json({
+            error: true,
+            message: 'Error: erro na consulta do artigo no banco de dados ' + error
+        })
+    })
+})
+
+//visualizando apenas um regitro na API
+app.get('/artigos/:id', (req, res)=>{
+    const artigo = Artigo.findOne({_id: req.params.id})
+    .then((artigo)=>{
+        return res.json(artigo)
+    }).catch((error)=>{
+        return res.status(400).json({
+            error: true,
+            message: 'Error: erro na consulta do artigo no banco de dados ' + error
+        })
+    })
+})
+
+// editando registro
+app.put('/artigos/:id', (req, res)=>{
+    const artigos = Artigo.updateOne({_id: req.params.id}, req.body)
+    .then((artigo)=>{
+        return res.status(400).json({
+            error: false,
+            message: 'Artigo editado com sucesso',
+        })
+    }).catch((error)=>{
+        return res.status(400).json({
+            error: true,
+            message: 'erro ao editar registro ' +error
+        })
+    })
+})
+
+app.delete('/artigo/:id', (req, res)=>{
+    const artigo = Artigo.deleteOne({_id: req.params.id})
+    .then(()=>{
+        return res.status(200).json({
+            error: false,
+            message: 'Artigo deletado com sucesso'
+        })
+    }).catch((erro)=>{
+        return res.status(400).json({
+            error: true,
+            message: 'erro ao artigo ser deletado '+erro
+        })
+    })
+})
+
+// cadastrando na API
+app.post('/artigo', (req, res)=>{
+    const artigo = Artigo.create(req.body)
+    .then(()=>{
+        return res.status(200).json({
+            error: false,
+            message: 'Artigo foi cadastrado com sucesso no banco de dados'
+        })
+    }).catch((error)=>{
+        return res.status(400).json({
+            error: true,
+            message: 'Error: Artigo não foi cadastrado no banco de dados ' + error
+        })
+    })
+})
+app.listen(8080, ()=>{
+    console.log('O servidor foi iniciado na porta 8080: http://localhost:8080')
+})
---
+++ models/Artigo.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Artigo = new mongoose.Schema({
+    titulo: {
+        type: String,
+        required: true
+    },
+    conteudo:{
+        type: String,
+        required: true
+    },
+},{
+    timestamps: true
+})
+
+mongoose.model('artigo', Artigo)
